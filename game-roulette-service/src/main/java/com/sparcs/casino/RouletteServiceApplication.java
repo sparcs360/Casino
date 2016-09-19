58088ee2fc55aea6a4b51406c2683c10b90df0fe
Lee Newfeld (Home) <lee.newfeld@gmail.com>
2016-09-19 19:13:19 +0100

    Temporarily disable jdbc/jpa autoconfig

--- game-roulette-service/src/main/java/com/sparcs/casino/RouletteServiceApplication.java
+++ game-roulette-service/src/main/java/com/sparcs/casino/RouletteServiceApplication.java
@@ -1,9 +1,11 @@
 package com.sparcs.casino;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//Temporarily disable database components
+//@SpringBootApplication
+@ComponentScan
 public class RouletteServiceApplication {
 
 	public static void main(String[] args) {
