088d6af23a6e5fd8137b40ec8be885a3e12a1e17
Lee Newfeld (Home) <lee.newfeld@gmail.com>
2016-09-25 17:46:00 +0100

    Temporarily disable JPA

--- game-roulette-service/src/main/java/com/sparcs/casino/RouletteServiceApplication.java
+++ game-roulette-service/src/main/java/com/sparcs/casino/RouletteServiceApplication.java
@@ -3,9 +3,12 @@ package com.sparcs.casino;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration 
+@ComponentScan
 public class RouletteServiceApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(RouletteServiceApplication.class);
@@ -18,4 +21,9 @@ public class RouletteServiceApplication {
 		app.run(args);
 		log.trace("-run()");
 	}
+	
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	
+        return application.sources(RouletteServiceApplication.class);
+    }
 }
